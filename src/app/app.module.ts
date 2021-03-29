b3f772fbab1488652f1ed98dc58d9a20217e927f
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-03-29 15:39:32 -0300

    Se trabajo en el componente de register, y refactorizacion de codigo en navbar y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,11 +20,9 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
 import { environment } from '../environments/environment';
-
-
-
 
 @NgModule({
   declarations: [
@@ -47,6 +45,7 @@ import { environment } from '../environments/environment';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
+    AngularFireStorageModule,
   ],
   providers: [AngularFireAuth],
   bootstrap: [AppComponent]
--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,46 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { AuthService } from '../../../services/auth.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  public email : string = '';
-  public password : string = '';
+  public email: string = '';
+  public password: string = '';
   
+  uploadPercent: Observable<number>;
+  urlImage: Observable<string>;
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private afStorage: AngularFireStorage,
+  ){}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRegister() {
+    this.authService.registerUser(this.email, this.password)
+      .then((res) => {
+        this.authService.loginRedirect();
+      }).catch(err => console.log('err', err.message));
   }
 
-  onRegister(){
-    this.authService.registerUser(this.email,this.password)
-    .then((res) => {
-      this.authService.loginRedirect();
-    }).catch( err => console.log('err',err.message));
-  }
-
-  onLoginGoogle(): void{
+  onLoginGoogle(): void {
     this.authService.loginGoogleUser()
-    .then( (res) => {
-      //console.log('resUser', res)
-      this.authService.loginRedirect();
-    }).catch ( err => console.log('error', err.message));
+      .then((res) => {
+        this.authService.loginRedirect();
+      }).catch(err => console.log('error', err.message));
   }
 
-  onLoginFacebook(): void{
+  onLoginFacebook(): void {
     this.authService.loginFacebookUser()
-    .then( (res) => {
-      //console.log('resUser', res)
-      this.authService.loginRedirect();
-    }).catch ( err => console.log('error', err.message));
+      .then((res) => {
+        this.authService.loginRedirect();
+      }).catch(err => console.log('error', err.message));
   }
 
-  onUpload(){}
+  onUpload(event) {
+    const id = Math.random().toString(36).substring(2);
+    const file = event.target.files[0];
+    const filePath = `uploads/profile_${id}`;
+    const ref = this.afStorage.ref(filePath);
+    const task = this.afStorage.upload(filePath, file);
+    this.uploadPercent = task.percentageChanges();
+    task.snapshotChanges().pipe(finalize(()=> this.urlImage = ref.getDownloadURL())).subscribe();
+  }
 }
